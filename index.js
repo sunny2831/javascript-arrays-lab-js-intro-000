188d9e298e1545b17ade1ac0c2f80e7b5dfe45f6
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-12 18:19:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAddElementToEndOfArray(name) {
+function destructivelyAppendKitten(name) {
   kittens.push(name)
   return kittens
 }
